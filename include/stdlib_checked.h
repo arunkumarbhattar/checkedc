917b1c5e95bb890eb2ab63a7c4cbda0113123c33
arunkumarbhattar <bhattar1@purdue.edu>
2022-05-11 00:00:27 -0400

    minor changes

--- include/stdlib_checked.h
+++ include/stdlib_checked.h
@@ -72,18 +72,21 @@ unsigned long long int strtoull(const char * restrict nptr :
 // TODO: express alignment constraints once where clauses have been added.
 void *aligned_alloc(size_t alignment, size_t size) : byte_count(size);
 _Itype_for_any(T) void *calloc(size_t nmemb, size_t size) : itype(_Array_ptr<T>) byte_count(nmemb * size);
+_Itype_for_any(T) _TArray_ptr<T> t_calloc(size_t nmemb, size_t size);
 _Itype_for_any(T) void free(void *pointer : itype(_Array_ptr<T>) byte_count(0));
+_Itype_for_any(T) void t_free(_TArray_ptr<T> pointer);
 _Itype_for_any(T) void *malloc(size_t size) : itype(_Array_ptr<T>) byte_count(size);
+_Itype_for_any(T) _TArray_ptr<T> t_malloc(size_t size);
 _Itype_for_any(T) void *realloc(void *pointer : itype(_Array_ptr<T>) byte_count(1), size_t size) : itype(_Array_ptr<T>) byte_count(size);
-
+_Itype_for_any(T) _TArray_ptr<T>realloc(_TArray_ptr<T>, size_t size);
 char *getenv(const char *n : itype(_Nt_array_ptr<const char>)) : itype(_Nt_array_ptr<char>);
 
 int atexit(void ((*func)(void)) : itype(_Ptr<void (void)>));
 int atquick_exit(void ((*func)(void)) : itype(_Ptr<void (void)>));
 
 int system(const char *s : itype(_Nt_array_ptr<const char>));
-
-// TODO: compar needs to have an itype that has bounds
+int t_system(_TNt_array_ptr<const char>);
+// TODO: compare needs to have an itype that has bounds
 // on parameters based on size.  Currently we are requiring that
 // bounds in parameters lists be closed with respect to variables
 // in the parameter list.
@@ -94,6 +97,12 @@ _Itype_for_any(T) void *bsearch(const void *key : itype(_Ptr<const T>),
                                   itype(_Ptr<int(_Ptr<const T>, _Ptr<const T>)>)
                                 ) : itype(_Ptr<T>);
 
+_Itype_for_any(T) _TPtr<T> t_bsearch(_TPtr<const T> key,
+                                _Array_ptr<const T> base,
+                                size_t nmemb, size_t size,
+                                _TPtr<int(_TPtr<const T>, _TPtr<const T>)>
+				);
+
 // TODO: compar needs to have an itype that has bounds
 // on parameters based on size.  Currently we are requiring that
 // types be closed.
@@ -102,12 +111,20 @@ void qsort(void *base : byte_count(nmemb * size),
            int ((*compar)(const void *, const void *)) :
              itype(_Ptr<int (_Ptr<const void>, _Ptr<const void>)>));
 
+_Itype_for_any(T) void t_qsort(_TArray_ptr<T> base : byte_count(nmemb * size),
+           size_t nmemb, size_t size,
+           _TPtr<int (_TPtr<const void>, _TPtr<const void>)>);
+
 int mblen(const char *s : count(n), size_t n);
+_Itype_for_any(T) int t_mblen(_TArray_ptr<const char> : count(n), size_t n);
 
 int mbtowc(wchar_t * restrict output : itype(restrict _Ptr<wchar_t>),
            const char * restrict input : count(n),
            size_t n);
 
+int t_mbtowc(restrict _TPtr<wchar_t>,
+           	restrict _TArray_ptr<const char> : count(n),
+           size_t n);
 // MB_CUR_MAX is a macro that becomes a function call, so is banned
 // in bounds annotations. 
 // 
@@ -118,9 +135,12 @@ size_t mbstowcs(wchar_t * restrict pwcs : count(n),
                   itype(restrict _Nt_array_ptr<const char>),
                 size_t n);
 
-size_t wcstombs(char * restrict output : count(n),
-                const wchar_t * restrict pwcs :
-                  itype(restrict _Nt_array_ptr<const wchar_t>),
+size_t t_mbstowcs(restrict _TArray<wchar_t> pwcs : count(n),
+                  restrict _Nt_array_ptr<const char>,
+                  size_t n);
+
+size_t t_wcstombs(restrict _TArray_ptr<char> output : count(n),
+                restrict _TNt_array_ptr<const wchar_t>,
                 size_t n);
 
 #pragma CHECKED_SCOPE pop
